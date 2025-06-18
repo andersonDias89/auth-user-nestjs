3abc51191b7f51c27f6ce346fb93f24d78ecb2f8
Anderson <andersondiasmd25@gmail.com>
2025-06-18 11:42:10 -0300

    configurando o typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,21 @@ import { ConfigModule } from '@nestjs/config';
     UserModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DB_TYPE === 'better-sqlite3') {
+          return {
+            type: process.env.DB_TYPE,
+            database: process.env.DB_DATABASE,
+            synchronize: process.env.DB_SYNCHRONIZE === '1',
+            autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+            // entities: [User, Post],
+          };
+        }
+        // Provide a default configuration or throw an error if DB_TYPE is not supported
+        throw new Error('Unsupported DB_TYPE or DB_TYPE not set');
+      },
     }),
   ],
   controllers: [AppController],
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,3 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class User {
+  @IsString({})
+  id: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
 }
